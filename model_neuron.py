6e5660554050a1286b9ea904fa61a320a56d703f
Malte <malte.esders@gmail.com>
2015-01-09 10:13:32 +0100

    Modified model neuron class

--- model_neuron.py
+++ model_neuron.py
@@ -1,1 +1,8 @@
 # This is the code for the neuron object! Not so much here yet :)
+
+class neuron:
+    'This is a model neuron class that takes input and gives output'    
+    
+    def __init__(self, projections):
+        # setting class parameters
+        self.projections = projections
