0b66303e1d7796b9d2dadcfdfa6967da5374f017
Malte <malte.esders@gmail.com>
2015-01-09 10:24:50 +0100

    added test script

--- model_neuron.py
+++ model_neuron.py
@@ -1,8 +1,11 @@
 # This is the code for the neuron object! Not so much here yet :)
 
-class neuron:
+class Neuron:
     'This is a model neuron class that takes input and gives output'    
     
     def __init__(self, projections):
         # setting class parameters
         self.projections = projections
+        
+    def get_projections(self):
+        return "My projections are: " + str(self.projections)
---
+++ test_to_run_project.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan  9 10:15:00 2015
+
+@author: maltimore
+"""
+import model_neuron
+
+neuron1 = Neuron(3)
