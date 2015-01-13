e84e0ff3d8cfec4663336697afae44c27f29986a
Malte <malte.esders@gmail.com>
2015-01-13 12:46:13 +0100

    added synapses, connections between neurons

--- model_neuron.py
+++ model_neuron.py
@@ -1,38 +1,37 @@
 # This is the code for the neuron object! Not so much here yet :)
 
 import numpy as np
+import model_synapse
 
 class Neuron:
     'This is a model neuron class that takes input and gives output'    
     
-    params = {\
-    'V': -.07,\
-    't': 0,\
-    'E_m': -.07,\
-    'refractory_p': .001,\
-    'tau_K': .015,\
-    'tau_m': .01,\
-    'tau_syn': 0.01,\
-    'R_m': 10**7,\
-    'g_max': 5e-8,\
-    'g_K': 36e-9,\
-    'E_K': -.077,\
-    'E_syn': 0,\
-    't_max': .07,\
-    'dt': .0001,\
-    'I_e': 0,\
-    'threshold': -.055\
-    }
+
 
     remaining_refractory = 0 # remaining absolute refractory period
     
 
-    def __init__(self, projections):
+    def __init__(self, neuronnumber):
         # setting class parameters
-        self.projections = projections
-        
-    def get_projections(self):
-        return "My projections are: " + str(self.projections)
+        self.params = {\
+        'V': -.07,\
+        't': 0,\
+        'E_m': -.07,\
+        'refractory_p': .001,\
+        'tau_K': .015,\
+        'tau_m': .01,\
+        'tau_syn': 0.01,\
+        'R_m': 10e7,\
+        'g_max': 5e-8,\
+        'g_K': 36e-9,\
+        'E_K': -.077,\
+        'E_syn': 0,\
+        't_max': .07,\
+        'dt': .0001,\
+        'I_e': 0,\
+        'threshold': -.055\
+        }
+        self.this_neurons_number = neuronnumber
         
     # This is the Euler step function, which will only compute one 
     # next timestep, therefore it is rather slim.
@@ -45,43 +44,92 @@ class Neuron:
         E_K = params['E_K']
         R_m = params['R_m']
         I_e = params['I_e']
-        t  = params['t']
         g_K = params['g_K']
             
         g_K = self.eulerstep(self.gK,g_K,self.params)        
         I_K = g_K*(V-E_K)        
         
-        return (-V + E_m - R_m * self.I_syn(V, t, params) + R_m * I_e - I_K) / tau_m
+
+    
+        I_syn = self.get_synaptic_input(V)
+        
+        return (-V + E_m - R_m * I_syn + R_m * I_e - I_K) / tau_m
+
+    def get_synaptic_input(self, V):
+        I_syn = 0
+        for synapse in self.synapse_list:
+            I_syn += synapse.get_I_syn(V)   
+        return I_syn
     
     def gK(self, g_K, params):
         tau_K = params['tau_K']
         return -g_K/tau_K
-        
-    def I_syn(self,V, t, params):
-        E_syn = params['E_syn']
-        
-        return self.g_syn(t, params['g_max'], params['tau_syn']) * (V - E_syn)
     
-    def g_syn(self,t, g_max, tau_syn):
-        return g_max * t / tau_syn * np.exp(-t/tau_syn)
-        
-    def update(self):
-        # check whether the neuron is in absolute refractory period, then set input currents to zero
-        if self.remaining_refractory > 0:
-            self.remaining_refractory -= self.params['dt'] # update remaining refractory
-            self.params['I_e'] = 0                         # set input to zero
-            
-        self.params['V'] = self.eulerstep(self.dV_dt,self.params['V'],self.params)
-        
-        if self.params['V'] > self.params['threshold']:
-            self.remaining_refractory = self.params['refractory_p']
-            
+                   
         
     def get_voltage(self):
         return self.params['V']
         
+        
     def set_external_current(self,new_Ie):
         self.params['I_e'] = new_Ie
+        
     
     def get_external_current(self):
         return self.params['I_e']
+        
+        
+    def set_input_connections(self, neuron_numbers, E_syn_vec):
+        self.input_connections = neuron_numbers
+        
+        #list that will hold input synapses:
+        self.synapse_list = []
+        
+        for i in np.arange(0,len(neuron_numbers)):
+            self.synapse_list.append(model_synapse.Synapse(self.params['g_max'], self.params['tau_syn'], \
+            self.params['dt'], E_syn_vec[i]))
+    
+    def set_output_connections(self, outputneurons):
+        self.postsynaptic_neurons = outputneurons
+    
+    
+    # receive synaptic input from other neuron        
+    def receive_synaptic_input(self, ext_neuron_number):    
+        # determine index of inputting neuron
+    #  LATER CHANGE THIS TO GET REAL INDEX
+        index = self.input_connections[self.input_connections == ext_neuron_number]
+        # start the synapse corresponding to inputting neuron
+        self.synapse_list[index].start()
+            
+            
+    def fire(self):
+        for neuron in self.postsynaptic_neurons:
+            neuron.receive_synaptic_input(self.this_neurons_number)
+        
+        # disable all input synapses    
+        for synapse in self.synapse_list:
+            synapse.disable()
+            
+        self.params['V'] = -.07
+        
+
+        
+            
+            
+    # this function is called by the test script to compute one timestep further    
+    def update(self):
+        
+        # check whether the neuron is in absolute refractory period, then set input currents to zero
+        # LATER HERE WE SHOULD JUST SET THE INPUT SYNAPSES TO ZERO     
+        if self.remaining_refractory > 0:
+            self.remaining_refractory -= self.params['dt'] # update remaining refractory
+
+            
+        self.params['V'] = self.eulerstep(self.dV_dt,self.params['V'],self.params)
+        
+        for synapse in self.synapse_list:
+            synapse.update()
+        
+        if self.params['V'] > self.params['threshold']:
+            self.remaining_refractory = self.params['refractory_p']
+            self.fire()
--- test_to_run_project.py
+++ test_to_run_project.py
@@ -9,16 +9,42 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-N = 500
+N = 1000
+n_neurons = 20
+my_linewidth = .1
 
-neuron1 = model_neuron.Neuron(3)
+
+volt_matrix = np.empty((n_neurons, N))
 
 voltage_vec = np.empty(N)
+random_neuron_sample = np.array([0 ,4  ,5  ,9  ,3])
+E_syn_list           = np.array([24 ,10 ,99 ,2  ,12])
+neuronlist = []
 
+# create neurons
+for i in np.arange(n_neurons):
+    neuronlist.append(model_neuron.Neuron(i))
+    
+for neuron in neuronlist:
+    neuron.set_input_connections(np.arange(0,n_neurons),np.ones((n_neurons)))
+    neuron.set_output_connections(neuronlist[:2])
+
+j = 0
 for i in np.arange(N):
-    neuron1.set_external_current(2e-9)
-    neuron1.update()
-    voltage_vec[i] = neuron1.get_voltage()
+    for neuron in neuronlist:
+        
+        neuron.set_external_current(np.random.random() * 3e-10)
+        
+        neuron.update()
+        
+        volt_matrix[j,i] = neuron.get_voltage()
+        j += 1
+    j = 0
 
 
-plt.plot(voltage_vec)
+for i in np.arange(n_neurons):
+    
+    plt.plot(volt_matrix[i], linewidth = my_linewidth)
+    
+plt.plot(volt_matrix[0], linewidth = 1)
+plt.plot(volt_matrix[4], linewidth = 1)
