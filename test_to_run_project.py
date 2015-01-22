383589db7ea16fb4833462318165f6a570b71b5d
Kreiswolke <oliver_eberle@web.de>
2015-01-22 15:10:40 +0100

    Ausgabe variance und anzahl spikes

--- test_to_run_project.py
+++ test_to_run_project.py
@@ -11,9 +11,9 @@ import numpy as np
 
 #####################################
 # Set constant variables
-N_timesteps = 500
-N_per_group = 100
-N_groups    = 4
+N_timesteps = 700
+N_per_group = 80
+N_groups    = 10
 N_neurons   = N_per_group * N_groups
 input_spikes = 50
 my_linewidth = .2
@@ -51,16 +51,28 @@ def give_initial_input(neuronlist, N_per_group, synchronization, N_spikes):
         artificial_input_neuron.set_output_connections(neuronlist[0:N_per_group])
         artificial_input_neuron.fire()
 
-def rasterplot():
+    
+#Calculate number of output spike and its variance
+def calculate_output_properties():
     eventmatrix = np.zeros((N_neurons,N_timesteps))
+    spiketimematrix = np.zeros((N_neurons,N_timesteps))
     for i in range(N_neurons):
         for j in range(len(neuronlist[i].spiketime)):
             eventmatrix[i][neuronlist[i].spiketime[j]/dt] = 1
-    plt.figure()
-    plt.xlabel('timestep')
-    plt.ylabel('neuron number')
-    plt.imshow(eventmatrix,cmap='gray_r')
+            spiketimematrix[i][neuronlist[i].spiketime[j]/dt] = neuronlist[i].spiketime[j]/dt
+    spikes_in_group=np.zeros(N_groups)
+    variance_in_group=np.zeros(N_groups)
+     
+    k=0
+    for  i in range (N_groups): 
+        spikes_in_group[k]=(np.sum(eventmatrix[(i)*N_per_group  :  (i+1)*N_per_group]))
+        hilf=spiketimematrix[(i)*N_per_group  :  (i+1)*N_per_group]
+        hilf=np.reshape(hilf,(np.shape(hilf)[0]*np.shape(hilf)[1]))
+        variance_in_group[k]=(np.std(np.trim_zeros(hilf)))
+        k+=1
+    return spikes_in_group, variance_in_group
 
+ 
 #####################################
 # Run simulation
 
@@ -82,8 +94,7 @@ for i in np.arange(N_neurons):
 plt.xlabel('time [ms]')
 plt.ylabel('voltage [V]')
 
-rasterplot()
-
+a_out, sig_out=calculate_output_properties()
 
 #N = 100000
 ## find best I_mu
